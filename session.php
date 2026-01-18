35911a121ba78ea2d49db54a9f36fe55d786f47d
innoventorysolutions-tech <innoventorysolutions@gmail.com>
2026-01-18 16:06:14 +0530

    init-checkin

--- session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// If not logged in → redirect to login
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("Location: index.php");
-    exit;
-}
-?>
